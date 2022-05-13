395e6bdc343525a41a2579ca7e4952eb42456094
baklovea <emrildes08@gmail.com>
2022-05-13 22:24:21 +0300

    new exercise

--- Functions/app.js
+++ Functions/app.js
@@ -1,5 +1,70 @@
+// function square (num){
+//     num *= num;
+//     return num;
+// }
+// console.log(square(5));
 
+// ---------------------------------------------------------------------------------
 
+// function square (num){
+//     num *= num;
+//     return num;
+// }
+
+// var squ = square(5)
+// console.log('number square = ' + squ);
+
+// ---------------------------------------------------------------------------------
+
+// You can take input from user
+
+// function square (num){
+//     num *= num;
+//     return num;
+// }
+
+// var squ = prompt('enter a number')
+// console.log('number square = ' + square(squ));
+
+// ---------------------------------------------------------------------------------
+
+// function text(i){
+//     let text_t = prompt('Please enter your text');
+//     let piece = prompt('Please enter you iteration number');
+//     for(var i = 1; i <= piece; i++){
+//         console.log(text_t)
+//     }
+// }
+
+// ---------------------------------------------------------------------------------
+
+// const students = ['john', 'jessica', 'marcos', 'emre']
+
+// function greet(student){
+//     console.log(`Welcome ${student}!`);
+// }
+
+// for(let i = 0; i < students.length; i++){
+//     greet(students[i]);
+// }
+    
+// ---------------------------------------------------------------------------------
+
+// const students = ['john', 'jessica', 'marcos', 'emre']
+
+// function greet(student){
+//     console.log(`Welcome ${student}!`);
+// }
+
+// function greetAll(who){
+//     for(let i = 0; i < who.length; i++){
+//     greet(who[i]);
+// }
+// }
+
+// greetAll(students)
+
+// ---------------------------------------------------------------------------------
 
 
 
